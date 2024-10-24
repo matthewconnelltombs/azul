d9867a24d225b114773143e0b29821f6b2182235
Matthew Connell-Tombs <matthewconnelltombs@gmail.com>
2024-10-24 08:48:11 -0400

    Tile Class

---
+++ Tile.py
@@ -0,0 +1,16 @@
+# Define tile colors
+TILE_COLORS = ['BLUE', 'WHITE', 'BLACK', 'RED', 'YELLOW']
+
+class Tile:
+    def __init__(self, color):
+        self.color = color
+
+    # Check for equality based on color.
+    def __eq__(self, other):
+        if isinstance(other, Tile):
+            return self.color == other.color
+        return False
+
+    def __str__(self):
+        return f"Tile (color={self.color})"
+
